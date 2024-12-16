f596dceeae43b96454a072b406b44bdfa9f03fe2
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2024-12-16 17:26:11 +0100

    feat: add script to run the challenge 02

---
+++ challenges/challenge_02/scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Arg 1: name of the network to use (chain-simulator, testnet, devnet)
+set -e
+
+mxops \
+    execute \
+    -n $1 \
+    -s mx-winter-coding-challenge \
+    mxops_scenes/01_generate_tokens.yaml
